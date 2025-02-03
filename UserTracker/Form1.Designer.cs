ed1d91a1e985e9aa70fa7250da7be958ecfe4c7e
Quixar <evgenmart01@gmail.com>
2025-02-03 23:52:10 +0200

    Добавьте файлы проекта.

---
+++ UserTracker/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace UserTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSettings = new Button();
+            btnModeration = new Button();
+            btnReports = new Button();
+            SuspendLayout();
+            // 
+            // btnSettings
+            // 
+            btnSettings.Location = new Point(555, 358);
+            btnSettings.Name = "btnSettings";
+            btnSettings.Size = new Size(75, 23);
+            btnSettings.TabIndex = 0;
+            btnSettings.Text = "Settings";
+            btnSettings.UseVisualStyleBackColor = true;
+            btnSettings.Click += btnSettings_Click;
+            // 
+            // btnModeration
+            // 
+            btnModeration.Location = new Point(435, 358);
+            btnModeration.Name = "btnModeration";
+            btnModeration.Size = new Size(82, 23);
+            btnModeration.TabIndex = 1;
+            btnModeration.Text = "Moderation";
+            btnModeration.UseVisualStyleBackColor = true;
+            // 
+            // btnReports
+            // 
+            btnReports.Location = new Point(330, 358);
+            btnReports.Name = "btnReports";
+            btnReports.Size = new Size(82, 23);
+            btnReports.TabIndex = 2;
+            btnReports.Text = "Reports";
+            btnReports.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnReports);
+            Controls.Add(btnModeration);
+            Controls.Add(btnSettings);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnSettings;
+        private Button btnModeration;
+        private Button btnReports;
+    }
+}
---
+++ UserTracker/Form1.cs
@@ -0,0 +1,15 @@
+namespace UserTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
